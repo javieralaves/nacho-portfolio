d696bfad60862a0b7eb39695bacf7bdb9e2554dc
Javier Alaves <jayalaves@gmail.com>
2021-07-07 18:41:08 +0200

    Updating text

--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,35 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My name is Nacho
+          Nacho is me
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>About me &rarr;</h2>
+            <p>I'm a cool guy based in Campello, Alicante.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Projects &rarr;</h2>
+            <p>Check out my projects with Next.js.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Experience &rarr;</h2>
+            <p>Download my CV to read about my work experience.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Contact me &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              I'm available for part-time freelancing.
             </p>
           </a>
         </div>
